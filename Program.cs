adae6e4bfb41263a43379ca43b73e11b55cfcee4
strw6 <strw6@SARAHGAMING>
2025-02-07 20:26:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_Three___Assignment_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
